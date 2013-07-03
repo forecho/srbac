2ff7d1b796089dc1658608c6019aa0f0806f9d42
forecho <caizhenghai@gmail.com>
2013-07-03 23:36:27 +0800

    Yii RBAC
    添加RBAC 还在配置当中

---
+++ protected/config/main.php
@@ -0,0 +1,133 @@
+<?php
+
+// uncomment the following to define a path alias
+// Yii::setPathOfAlias('local','path/to/local-folder');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'My Web Application',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	'timeZone' => 'Asia/Shanghai',
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+	),
+
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		/*
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'Enter Your Password Here',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+		),
+		*/
+		'srbac' => array(
+			'userclass'=>'User', //default: User 
+			'userid'=>'user_ID', //default: userid 
+			'username'=>'username', //default:username 
+			'delimeter'=>'@', //default:-
+			'debug'=>true, //default :false
+			'pageSize'=>10, // default : 15
+			'superUser' =>'Authority', //default: Authorizer 
+			'css'=>'srbac.css', //default: srbac.css 
+			'layout'=>'application.views.layouts.main', //default: application.views.layouts.main,
+			//must be an existing alias 
+			'notAuthorizedView'=> 'srbac.views.authitem.unauthorized', // default:
+			//srbac.views.authitem.unauthorized, must be an existing alias 
+			'alwaysAllowed'=>array( //default: array() 
+				'SiteLogin','SiteLogout','SiteIndex','SiteAdmin', 
+				'SiteError', 'SiteContact'),
+			'userActions'=>array('Show','View','List'), //default: array() 
+			'listBoxNumberOfLines' => 15, //default : 10
+			'imagesPath' => 'srbac.images', // default: srbac.images 
+			'imagesPack'=>'noia', //default: noia
+			'iconText'=>true, // default : false 
+			'header'=>'srbac.views.authitem.header', //default : srbac.views.authitem.header,                                                   
+			//must be an existing alias 
+			'footer'=>'srbac.views.authitem.footer', //default: srbac.views.authitem.footer,                                                   
+			//must be an existing alias 
+			'showHeader'=>true, // default: false         
+			'showFooter'=>true, // default: false         
+			'alwaysAllowedPath'=>'srbac.components', // default: srbac.components                                                  
+			// must be an existing alias 
+
+	),
+
+	// application components
+	'components'=>array(
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+		// uncomment the following to enable URLs in path-format
+		/*
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		*/
+		// 'db'=>array(
+		// 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		// ),
+		// uncomment the following to use a MySQL database
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=srbac',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+		),
+		'authManager'=>array(
+		        // 类SDbAuthManager在srbac模块中的路径（别名），注意大小写
+		        'class'=>'modules.srbac.components.SDbAuthManager',
+		        // 使用的数据库的组件名        
+		        'connectionID'=>'db',
+		        // 下面是3个数据表，后面再说每个表的作用
+		        // The itemTable name (default:authitem)
+		        'itemTable'=>'items',         
+		        // The assignmentTable name (default:authassignment)         
+		        'assignmentTable'=>'assignments',         
+		        // The itemChildTable name (default:authitemchild)         
+		        'itemChildTable'=>'itemchildren',       
+		), 
+
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>'site/error',
+		),
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
+);
